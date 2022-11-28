016a681ef4bbf02c0e1ae67b34d61f33da35466e
Stephen FitzSimon <stephen.fitzsimon@gmail.com>
2022-11-28 09:45:49 -0600

    figuring out how to correctly display text

---
+++ text_test.py
@@ -0,0 +1,29 @@
+from scripture_memory import LINES_TO_DISPLAY
+
+
+LINES_TO_DISPLAY = 5
+
+def main():
+    txt = get_text('example_text.txt')
+    current_line_idx = 1
+    while current_line_idx < len(txt):
+        if current_line_idx < LINES_TO_DISPLAY:
+            start = 0
+        else:
+            start = current_line_idx - LINES_TO_DISPLAY
+        prev_lines = txt[start:current_line_idx]
+        current_line = txt[current_line_idx]
+
+        print(''.join(prev_lines))
+        user_text = input()
+        
+        current_line_idx += 1
+
+def get_text(filename):
+    txt = ['Beginning\n']
+    with open(filename) as f:
+        txt += [l for l in f]
+    return txt
+
+if __name__=='__main__':
+    main()
