6be2031e3777674d2719c168f7b4e3cf958afefc
Stephen FitzSimon <stephen.fitzsimon@gmail.com>
2022-12-02 13:54:50 -0600

    up to getting new screen

--- wpm_tutorial.py
+++ wpm_tutorial.py
@@ -1,17 +1,28 @@
 import curses
 from curses import wrapper
 
+def start_screen(stdscr):
+    stdscr.clear()
+    stdscr.addstr("This is a wpm test program.")
+    stdscr.addstr("\nPress any key to begin...")
+    stdscr.refresh()
+    stdscr.getkey()
+
+def wpm_test(stdscr):
+    target_text = "Hello World! This is example text"
+    current_text = []
+
+    stdscr.clear()
+    stdscr.addstr(target_text)
+    stdscr.refresh()
+    stdscr.getkey()
+
 def main(stdscr):
     curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_BLACK)
     curses.init_pair(2, curses.COLOR_RED, curses.COLOR_BLACK)
     curses.init_pair(3, curses.COLOR_WHITE, curses.COLOR_BLACK)
 
-    stdscr.clear()
-    stdscr.addstr(1, 5, "Here is printed text", curses.color_pair(1))
-
-    stdscr.addstr(1, 0, "Here is printed text", curses.color_pair(1))
-    stdscr.refresh()
-
-    stdscr.getkey()
+    start_screen(stdscr)
+    wpm_test(stdscr)
 
 wrapper(main)
